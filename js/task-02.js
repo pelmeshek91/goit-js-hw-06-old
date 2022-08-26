95db9d2eb8b9bd8b7305ff1958fc79370370d45a
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-08-26 14:11:57 +0100

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+const addIngredients = ingredients.map((ingredient) => {
+  const itemIngredients = document.createElement("li");
+  itemIngredients.textContent = `${ingredient}`;
+  itemIngredients.classList.add("item");
+  return itemIngredients;
+});
+listIngredients.append(...addIngredients);
