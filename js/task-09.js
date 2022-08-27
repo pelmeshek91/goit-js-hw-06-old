e01d84cbc3014d493ce698aee4f62b74a0b84cfc
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-08-27 20:51:20 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+document.querySelector(".change-color").addEventListener("click", function (e) {
+  e.preventDefault();
+  const body = document.querySelector("body");
+  let color = getRandomHexColor();
+  document.querySelector(".color").innerHTML = color;
+  body.style.backgroundColor = color;
+});
